5603aab3e8cf434881d8814e16a7609340b0755f
huxiaoningsworld <huxiaoningsworld@sina.com>
2024-05-07 17:20:02 +0800

    xxx

--- p.asm
+++ p.asm
@@ -1,10 +1,10 @@
-assume cs:abc
-abc segment
+assume cs:code
+code segment
     start:mov ax,2
           add ax,ax
           add ax,ax
 	
           mov ax,4c00H    ;程序返回
           int 21H         ;程序返回
-abc ends
+code ends
 end start
