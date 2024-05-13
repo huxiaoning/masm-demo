26b558240d49e0fd93a25e34c4f56c435a246a0e
huxiaoningsworld <huxiaoningsworld@sina.com>
2024-05-13 20:38:20 +0800

    xxx

---
+++ code/chapter12/p12.1.asm
@@ -0,0 +1,12 @@
+assume cs:code
+code segment
+    start:                ; do0 安装程序
+    ; 设置中断向量表
+          mov ax,4c00h
+          int 21h
+
+    ; do0: 显示字符串"overflow!"
+          mov ax,4c00h
+          int 21h
+code ends
+end start
