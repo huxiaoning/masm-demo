cc776d750db55aabbc3248548aa33855c161b983
huxiaoningsworld <huxiaoningsworld@sina.com>
2024-05-13 20:45:08 +0800

    xxx

--- code/chapter12/p12.8.asm
+++ code/chapter12/p12.8.asm
@@ -1,12 +1,18 @@
 assume cs:code
 code segment
-    start:                ; do0 安装程序
-    ; 设置中断向量表
-          mov ax,4c00h
-          int 21h
+      start:
+      ; 设置 es：di 指向目的地址
+      ; 设置 ds：si 指向源地址
+      ; 设置 cx 为传输长度
+      ; 设置传输方向为正
+            rep movsb
 
-    ; do0: 显示字符串"overflow!"
-          mov ax,4c00h
-          int 21h
+      ; 设置中断向量表
+            mov ax,4c00h
+            int 21h
+
+      ; do0: 显示字符串"overflow!"
+            mov ax,4c00h
+            int 21h
 code ends
 end start
