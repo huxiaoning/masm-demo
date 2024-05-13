3435b61cfe35f78567449ab2e698e3d91529f2f7
huxiaoningsworld <huxiaoningsworld@sina.com>
2024-05-13 20:52:13 +0800

    xxxx

--- code/chapter12/p12.8.asm
+++ code/chapter12/p12.8.asm
@@ -3,15 +3,15 @@ code segment
       start:
             mov ax,cs
             mov ds,ax
-            mov si,offset do0      ; 设置 ds:si 指向源地址
+            mov si,offset do0                       ; 设置 ds:si 指向源地址
 
             mov ax,0
             mov es,ax
-            mov di,200H            ; 设置 es:di 指向目的地址
+            mov di,200H                             ; 设置 es:di 指向目的地址
 
-            mov cx,do0部分代码的长度      ; 设置 cx 为传输长度
+            mov cx,offset do0end -  offset do0      ; 设置 cx 为传输长度
       
-            cld                    ; 设置传输方向为正
+            cld                                     ; 设置传输方向为正
             rep movsb
 
       ; 设置中断向量表
@@ -21,5 +21,6 @@ code segment
       ; do0: 显示字符串"overflow!"
             mov ax,4c00h
             int 21h
+      ; do0end:nop
 code ends
 end start
