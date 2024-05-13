21d4e338ff84405eaec284cbaef49ec325a1a73f
huxiaoningsworld <huxiaoningsworld@sina.com>
2024-05-13 20:44:04 +0800

    xxx

--- code/chapter12/p12.8.asm
+++ code/chapter12/p12.8.asm
@@ -1,12 +1,6 @@
 assume cs:code
 code segment
-    start:
-    ; 设置 es：di 指向目的地址
-    ; 设置 ds：si 指向源地址
-    ; 设置 cx 为传输长度
-    ; 设置传输方向为正
-          rep movsb
-
+    start:                ; do0 安装程序
     ; 设置中断向量表
           mov ax,4c00h
           int 21h
