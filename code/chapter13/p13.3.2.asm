a336a31f495836dff36d3100d0fad0c787263653
huxiaoningsworld <huxiaoningsworld@sina.com>
2024-05-20 08:49:36 +0800

    xxx

--- code/chapter13/p13.3.2.asm
+++ code/chapter13/p13.3.2.asm
@@ -6,10 +6,10 @@ code segment
       start:mov ax,data
             mov ds,ax
             mov si,0
-            mov ax,Ob800h
+            mov ax,0b800h
             mov es,ax
             mov di,12*160
-      S:    cmp byte ptr [si],0
+      s:    cmp byte ptr [si],0
             je  ok                         ; 如果是 0 跳出循环
             mov al, [si]
             mov es:[di],al
@@ -18,7 +18,7 @@ code segment
             mov bx,offset s-offset ok      ; 设置从标号ok到标号s的转移位移
             int 7ch                        ; 转移到标号s处
       ok:   
-            int 21h
             mov ax,4c00h
+            int 21h
 code ends
 end start
--- code/chapter13/p13.3.2.setup.asm
+++ code/chapter13/p13.3.2.setup.asm
@@ -1,5 +1,28 @@
 assume cs:code
 code segment
+      start:          mov  ax,cs
+                      mov  ds,ax
+                      mov  si,offset jumpnearptrs
+                      mov  ax,0
+                      mov  es,ax
+                      mov  di,200h
+                      mov  cx,offset jumpnearptrsend-offset jumpnearptrs
+                      cld
+                      rep  movsb
 
+                      mov  ax,0
+                      mov  es,ax
+                      mov  word ptr es: [7ch*4],200h
+                      mov  word ptr es: [7ch*4+2],0
+
+                      mov  ax,4c00h
+                      int  21h
+
+      jumpnearptrs:   push bp
+                      mov  bp,sp
+                      add  [bp+2],bx
+                      pop  bp
+                      iret
+      jumpnearptrsend:nop
 code ends
 end start
